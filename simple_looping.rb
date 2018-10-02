94d8124337c17af2710bd8b8cb7eb8e24f649be0
John Sime <johnc.sime@gmail.com>
2018-10-02 04:05:32 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -32,7 +32,7 @@ def while_iterator(number_of_times)
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
   greeting = 0
-  while greeting < 7
+  while greeting <= 7
     puts phrase
   end
 
